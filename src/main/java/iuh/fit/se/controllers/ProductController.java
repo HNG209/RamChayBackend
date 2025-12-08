c75c7a14052193d168d989b6690d76d6566821a0
NgocDung (Co Phuong Hang) <Dungheongunhubo>
2025-12-08 21:03:33 +0700

    update product

--- src/main/java/iuh/fit/se/controllers/ProductController.java
+++ src/main/java/iuh/fit/se/controllers/ProductController.java
@@ -27,69 +27,69 @@ import java.util.Set;
 @CrossOrigin(origins = "*")
 @RequiredArgsConstructor
 public class ProductController {
-//    private final ProductService productService;
-//    private final CloudinaryService cloudinaryService;
-//    private final ObjectMapper objectMapper;
-//
-//    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-//    public ApiResponse<ProductCreationResponse> addProduct(
-//            @Parameter(schema = @Schema(implementation = ProductCreationRequest.class))
-//            @RequestPart("product") String productString,
-//            @RequestPart("image") List<MultipartFile> images) throws IOException {
-//
-//        ProductCreationRequest request = null;
-//        try {
-//            request = objectMapper.readValue(productString, ProductCreationRequest.class);
-//        } catch (Exception e) {
-//            throw new RuntimeException("Dữ liệu JSON sản phẩm không hợp lệ!");
-//        }
-//
-////        request.setMediaUploadRequests(mediaSet);
-////        request.setImageUrl(null);
-//
-//        return ApiResponse.<ProductCreationResponse>builder()
-//                .result(productService.createProduct(request, images))
-//                .build();
-//    }
-//
-//    @GetMapping
-//    public ApiResponse<List<ProductCreationResponse>> getAllProducts() {
-//        return ApiResponse.<List<ProductCreationResponse>>builder()
-//                .result(productService.getAllProducts())
-//                .build();
-//    }
-//
-//    @GetMapping("/{id}")
-//    public ApiResponse<ProductCreationResponse> getProductById(@PathVariable Long id) {
-//        return ApiResponse.<ProductCreationResponse>builder()
-//                .result(productService.getProductById(id))
-//                .build();
-//    }
-//
-//    @DeleteMapping("/{id}")
-//    public ApiResponse<String> deleteProduct(@PathVariable Long id) throws IOException {
-//        productService.deleteProduct(id);
-//        return ApiResponse.<String>builder()
-//                .result("Xóa sản phẩm thành công")
-//                .build();
-//    }
-//
-//    @PutMapping(value = "/{id}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-//    public ApiResponse<ProductCreationResponse> updateProduct (
-//            @PathVariable Long id,
-//            @Parameter(schema = @Schema(implementation = ProductCreationRequest.class))
-//            @RequestPart("product") String productString,
-//            @RequestPart(value = "image", required = false) List<MultipartFile> images) throws IOException {
-//
-//        ProductCreationRequest request = null;
-//        try {
-//            request = objectMapper.readValue(productString, ProductCreationRequest.class);
-//        } catch (Exception e) {
-//            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Dữ liệu JSON sản phẩm không hợp lệ!");
-//        }
-//
-//        return ApiResponse.<ProductCreationResponse>builder()
-//                .result(productService.updateProduct(id, request, images))
-//                .build();
-//    }
+    private final ProductService productService;
+    private final CloudinaryService cloudinaryService;
+    private final ObjectMapper objectMapper;
+
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ApiResponse<ProductCreationResponse> addProduct(
+            @Parameter(schema = @Schema(implementation = ProductCreationRequest.class))
+            @RequestPart("product") String productString,
+            @RequestPart("image") List<MultipartFile> images) throws IOException {
+
+        ProductCreationRequest request = null;
+        try {
+            request = objectMapper.readValue(productString, ProductCreationRequest.class);
+        } catch (Exception e) {
+            throw new RuntimeException("Dữ liệu JSON sản phẩm không hợp lệ!");
+        }
+
+//        request.setMediaUploadRequests(mediaSet);
+//        request.setImageUrl(null);
+
+        return ApiResponse.<ProductCreationResponse>builder()
+                .result(productService.createProduct(request, images))
+                .build();
+    }
+
+    @GetMapping
+    public ApiResponse<List<ProductCreationResponse>> getAllProducts() {
+        return ApiResponse.<List<ProductCreationResponse>>builder()
+                .result(productService.getAllProducts())
+                .build();
+    }
+
+    @GetMapping("/{id}")
+    public ApiResponse<ProductCreationResponse> getProductById(@PathVariable Long id) {
+        return ApiResponse.<ProductCreationResponse>builder()
+                .result(productService.getProductById(id))
+                .build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ApiResponse<String> deleteProduct(@PathVariable Long id) throws IOException {
+        productService.deleteProduct(id);
+        return ApiResponse.<String>builder()
+                .result("Xóa sản phẩm thành công")
+                .build();
+    }
+
+    @PutMapping(value = "/{id}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ApiResponse<ProductCreationResponse> updateProduct (
+            @PathVariable Long id,
+            @Parameter(schema = @Schema(implementation = ProductCreationRequest.class))
+            @RequestPart("product") String productString,
+            @RequestPart(value = "image", required = false) List<MultipartFile> images) throws IOException {
+
+        ProductCreationRequest request = null;
+        try {
+            request = objectMapper.readValue(productString, ProductCreationRequest.class);
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Dữ liệu JSON sản phẩm không hợp lệ!");
+        }
+
+        return ApiResponse.<ProductCreationResponse>builder()
+                .result(productService.updateProduct(id, request, images))
+                .build();
+    }
 }
